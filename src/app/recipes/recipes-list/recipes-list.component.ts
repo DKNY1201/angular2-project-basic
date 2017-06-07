3665298666a71ebd0246c619534ebfbd8e266ce4
Quy Tran <quytran288@gmail.com>
2017-06-07 09:45:32 -0500

    - Using components - Create recipe model

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes : Recipe[] = [
+    new Recipe('Chicken Sause', 'Chicken with sauce', 'https://www.fiveyou.com/wp-content/uploads/2017/04/Soy_Sauce_Chicken.jpg'),
+    new Recipe('Pork meat', 'Pork meat with flavor', 'http://www.seriouseats.com/recipes/assets_c/2014/08/20140731-meat-hook-meat-book-chinese-bbq-pork-michael-harlan-turkell-thumb-1500xauto-408548.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
